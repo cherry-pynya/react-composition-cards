b5747fa5cfc441d3d18b6b1e9a34ee5085cea259
chery-pynya <nikita.cherepnya@gmail.com>
2021-08-09 22:10:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card width={'18rem'}>
+        <CardImg name='some' />
+        <CardBody>
+          <CardTitle title='Some Title' />
+          <CardSubTitle subTittle='Some subtittle' />
+          <CardText text='Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem' />
+          <CardLink text='link text' />
+        </CardBody>
+      </Card>
+      <Card width={'18rem'}>
+        <CardTitle title='Some Title' />
+        <CardText text='Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem' />
+      </Card>
     </div>
+  );
+}
+
+function CardImg({name}) {
+  return (
+      <img className='card-img-top' src='https://picsum.photos/200/300' alt={name}></img>
+  );
+}
+
+function CardTitle({title}) {
+  return (
+      <h5 className='card-title'>{title}</h5>
+  );
+}
+
+function CardSubTitle({subTittle}) {
+  return (
+      <h6 className='card-subtitle mb-2 text-muted'>{subTittle}</h6>
+  );
+}
+
+function CardText({text}) {
+  return (
+      <p className='card-text'>{text}</p>
+  );
+}
+function CardLink({text}) {
+  return (
+      <a href='#' className='card-link'>{text}</a>
+  );
+}
+
+function CardBody({...props}) {
+  return (
+      <div className='card-body'>
+        {props.children}
+      </div>
+  );
+}
+
+function Card({width, ...props}) {
+  return (
+      <div className='card' style={{width: width}}>
+        {props.children}
+      </div>
   );
 }
 
